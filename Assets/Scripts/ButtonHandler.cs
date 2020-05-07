6c6270afa065a51360c2ae35b1a83c88cc24b22c
ronakp98 <ronak1998@gmail.com>
2020-05-07 02:06:26 -0400

    Update ButtonHandler.cs
    Included updated writing for the script

--- Assets/Scripts/ButtonHandler.cs
+++ Assets/Scripts/ButtonHandler.cs
@@ -29,7 +29,7 @@ public class ButtonHandler : MonoBehaviour
 
 		animator = GetComponent<Animator>();
 
-		explnText.text = "Welcome!!\n This application will show you the basics of object-oriented concepts through the use of AR. The first topic will be about classes and objects.";
+		explnText.text = "Welcome!!\n This application will show you the basic concepts of object-oriented programming through the use of Augmented Reality (AR). The first topic will be on classes and objects.";
 		activeText.text = "";
 	}
 
@@ -40,7 +40,7 @@ public class ButtonHandler : MonoBehaviour
 		switch (clicks)
 		{
 			case 1:
-				text = "Before pressing the NEXT button please scan the image target\nThe image target for the first lesson will say \n'Classes and Objects'";
+				text = "Before pressing the NEXT button please scan your flashcard target\nThe flashcard for the first lesson is \n'Classes and Objects'";
 				explnText.text = text;
 				break;
 			case 2:
@@ -50,7 +50,7 @@ public class ButtonHandler : MonoBehaviour
 			case 3:
 				animator.Play("cube Appear");
 				textObject.SetActive(true);
-				text = "Let's imagine that this blueprint car represents a car class. The fields represent different attributes that are associated with the car class and can hold information for the class";
+				text = "Let's imagine that this blueprint car represents a car class. The fields represent different attributes of the car class, which can hold information about the car";
 				explnText.text = text;
 				break;
 			case 4:
@@ -59,26 +59,26 @@ public class ButtonHandler : MonoBehaviour
 				break;
 			case 5:
 				//yellowCar appears
-				text = "Since we now understand what each part of the car class does, let's create a yellow car from this blueprint car class. This yellowCar object has its fields defined for its attributes above the object";
+				text = "Since we now understand what each part of the car class does, let's create an object yellow car from this blueprint car class. This yellowCar object has its fields from the class, which are defined for its attributes, shown above the object";
 				explnText.text = text;
 				break;
 			case 6:
 				//greenCar appears
-				text = "Now that we have created a yellowCar object, let's create another car object and call it greenCar. This car object has defined its fields to its own attributes seen above the greenCar object.";
+				text = "Now that we have created a yellowCar object, let's create another car object and call it greenCar. This car object has defined its fields to its own attributes shown above the greenCar object";
 				explnText.text = text;
 				break;
 			case 7:
-				text = "Now we have created two seperate car objects, both were created from the same blueprint car class. Both cars have access to the methods listed in the car class.\n Let's call a few of these methods";
+				text = "Now we have created two separate car objects, both were created from the same blueprint car class. Both cars objects have access to the methods listed in the car class.\n Let’s call a few of these methods";
 				explnText.text = text;
 				break;
 			case 8:
-				text = "The first method that is defined in the car class is accelerate(). This method allows the car object to move forward. Let's call this method for both of the cars and observe the results";
+				text = "The first method that is defined in the car class is accelerate(). This method allows the car objects to move forward. Let's call this method for both of the cars and observe the results";
 				explnText.text = text;
 				break;
 			case 9:
 				//Both cars should accelerate forward
 				activeText.text = "accelerate()";
-				text = "We could observe that both cars moved forward when the accelerate() method was called. Next we should try calling the reverse() method for both car objects.";
+				text = "We observe that both cars moved forward when the accelerate() method was called. Next, we should try calling the reverse() method for both car objects.";
 				explnText.text = text;
 				break;
 			case 10:
@@ -101,27 +101,26 @@ public class ButtonHandler : MonoBehaviour
 				break;
 			case 13:
 				activeText.text = "";
-				text = "We had one main car class that declared the fields and methods. We used this blueprint car class as a foundation to create multiple real life car objects with different attributes";
+				text = "We had one car class that declared the fields and methods. We used this blueprint car class as a foundation to create multiple real-life car objects with different attributes, which can access the different methods of the class";
 				explnText.text = text;
 				break;
 			case 14:
-				text = "This was a basic implementation of classes and objects. This understanding can be adapted to many other examples as well:\nAnimal Class\nBook Class";
+				text = "This was a basic implementation of classes and objects. This understanding can be adapted to many other examples as well, such as: \nAnimal Class\nBook Class";
 				explnText.text = text;
 				break;
 			case 15:
-				text = "This object-oriented concept should be used throughout your programming career as it is a fundamental computing concept.";
+				text = "This object-oriented concept could be used throughout your programming career as it is a fundamental computing concept.";
 				explnText.text = text;
                 //Change next button to next
 				nextButton.GetComponentInChildren<Text>().text = "Next";
 				break;
 			case 16:
-				text = "This is the end of the lesson! Hope you learned the concept of this lesson.\n Press Quit to return to the Main Menu or press the button in the top right to discover more examples";
+				text = "This is the end of the lesson! Hope you learned the concept of this lesson.\n Press Quit to return to the Main Menu or press the button in the top right to discover more examples of a class!";
 				explnText.text = text;
 				//Change next button to quit
 				nextButton.GetComponentInChildren<Text>().text = "Quit";
 				break;
 			case 17:
-				//Where I make them go back to main menu STILL NEED TO CODE THIS
 				SceneManager.LoadScene(0);
 				break;
 			default:
@@ -141,7 +140,7 @@ public class ButtonHandler : MonoBehaviour
 				clicks = 0;
 				break;
 			case 1:
-				text = "Before pressing the NEXT button please scan the image target\nThe image target for the first lesson will be the face of a dollar bill";
+				text = "Before pressing the NEXT button please scan your flashcard target\nThe flashcard for the first lesson is \n'Classes and Objects'";
 				explnText.text = text;
 				break;
 			case 2:
@@ -151,35 +150,35 @@ public class ButtonHandler : MonoBehaviour
 			case 3:
 				animator.Play("cube Appear");
 				textObject.SetActive(true);
-				text = "Let's imagine that this blueprint car represents a car class. The fields represent different variables that are associated with the car class and can hold information for the class";
+				text = "Let's imagine that this blueprint car represents a car class. The fields represent different attributes of the car class, which can hold information about the car";
 				explnText.text = text;
 				break;
 			case 4:
-				text = "The methods represent different functions that are defined inside of the class. These methods can allow the objects created from the class to carry out certain actions";
+				text = "The methods represent different functions that are defined inside the class. These methods can allow the objects created from the class to carry out certain actions";
 				explnText.text = text;
 				break;
 			case 5:
 				//yellowCar appears
-				text = "Since we now understand what each part of the car class does, let's create a yellow car from this blueprint car class. This yellowCar object will have its fields defined for its specific attributes";
+				text = "Since we now understand what each part of the car class does, let's create an object yellow car from this blueprint car class. This yellowCar object has its fields from the class, which are defined for its attributes, shown above the object";
 				explnText.text = text;
 				break;
 			case 6:
 				//greenCar appears
-				text = "Now that we have created a yellowCar object, let's create another car object and call it greenCar. This car object will define its fields according to its own attributes.";
+				text = "Now that we have created a yellowCar object, let's create another car object and call it greenCar. This car object has defined its fields to its own attributes shown above the greenCar object";
 				explnText.text = text;
 				break;
 			case 7:
-				text = "Now we have created two seperate car objects, both were created from the same blueprint car class. Both cars have access to the methods listed in the car class.\n Lets calling a few of these methods";
+				text = "Now we have created two separate car objects, both were created from the same blueprint car class. Both cars objects have access to the methods listed in the car class.\n Let’s call a few of these methods";
 				explnText.text = text;
 				break;
 			case 8:
-				text = "The first method that is defined in the car class is accelerate(). This method allows the car object to move forward. Let's call this method for both of the cars and observe the results";
+				text = "The first method that is defined in the car class is accelerate(). This method allows the car objects to move forward. Let's call this method for both of the cars and observe the results";
 				explnText.text = text;
 				break;
 			case 9:
 				//Both cars should accelerate forward
 				activeText.text = "accelerate()";
-				text = "We could observe that both cars moved forward when the accelerate() method was called. Next we should try calling the reverse() method for both car objects.";
+				text = "We observe that both cars moved forward when the accelerate() method was called. Next, we should try calling the reverse() method for both car objects.";
 				explnText.text = text;
 				break;
 			case 10:
@@ -202,27 +201,28 @@ public class ButtonHandler : MonoBehaviour
 				break;
 			case 13:
 				activeText.text = "";
-				text = "We had one main car class that initialized the fields and methods. We used this blueprint car class as a foundation to create multiple car objects with different field values";
+				text = "We had one car class that declared the fields and methods. We used this blueprint car class as a foundation to create multiple real-life car objects with different attributes, which can access the different methods of the class";
 				explnText.text = text;
 				break;
 			case 14:
-				text = "This was a basic implementation of Object Oriented Programming. This understadning can be adapted to many other examples as well:\nAnimal Class\nBook Class\nVideo Game Class";
+				text = "This was a basic implementation of classes and objects. This understanding can be adapted to many other examples as well, such as: \nAnimal Class\nBook Class";
 				explnText.text = text;
 				break;
 			case 15:
-				text = "This concept should be used throughout your programming career as it is a fundamental computing concept.";
+				text = "This object-oriented concept could be used throughout your programming career as it is a fundamental computing concept.";
 				explnText.text = text;
-                //Change next button back to next
+				//Change next button to next
 				nextButton.GetComponentInChildren<Text>().text = "Next";
 				break;
 			case 16:
-				text = "This is the end of the lesson! Hope you learned the concept of this lesson.\n Press Quit to return to the main menu";
+				text = "This is the end of the lesson! Hope you learned the concept of this lesson.\n Press Quit to return to the Main Menu or press the button in the top right to discover more examples of a class!";
 				explnText.text = text;
-                //Change next button to quit
+				//Change next button to quit
 				nextButton.GetComponentInChildren<Text>().text = "Quit";
 				break;
 			case 17:
-				//Code the quit button to exit them out STILL NEED TO CODE
+				SceneManager.LoadScene(0);
+				break;
 			default:
 
 				break;
